202c0ad0ed368760e9e017cee3e1c38d6b2d3e53
topias <topias@mariapori.fi>
2022-11-26 22:06:35 +0200

    CLI Argument for quick generating

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
+use std::env::args;
 use rand::Rng;
-const PASSWORD_LENGHT: i32 = 12;
 const CHARS: &str = "abcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$";
+const PASSWORD_LENGHT: i32 = 12;
 fn main() {
+    if(args().len() > 1){
+        let lenght: i32 = args().nth(1).unwrap().parse::<i32>().unwrap_or_default();
+        if(lenght == 0){
+            println!("Please input a number.");
+            return;
+        }
+        let mut rng = rand::thread_rng();
+        let mut i = 0;
+        for i in 0..lenght {
+            let mut rng = rand::thread_rng();
+            let random = rng.gen_range(0..CHARS.len());
+            let char = CHARS.chars().nth(random).unwrap();
+            print!("{}", char);
+        }
+        print!("\n");
+        return;
+    }
     println!("***AuroraPass**************");
     print!("Your password: ");
     let mut rng = rand::thread_rng();
